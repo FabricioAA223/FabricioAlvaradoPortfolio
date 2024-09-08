73ba634c4b1584884361dfa822d4cc83adabc7aa
FabricioAA223 <fabricioalvarado223@estudiantec.cr>
2024-09-07 19:15:04 -0600

    My skills added

--- script.js
+++ script.js
@@ -19,32 +19,45 @@ function navbarOption(e){
     document.getElementById('div-portfolio').style.display = "none";
     document.getElementById('div-blog').style.display = "none";
     document.getElementById('div-contact').style.display = "none";
+
+    document.getElementById('navbar-home').style.color = 'white'
+    document.getElementById('navbar-about').style.color = 'white'
+    document.getElementById('navbar-portfolio').style.color = 'white'
+    document.getElementById('navbar-blog').style.color = 'white'
+    document.getElementById('navbar-contact').style.color = 'white'
+
     switch (navbarOptionSelected){
         case 'Home':
             document.getElementById('div-home').style.display = "flex";
             document.getElementById('section-name').innerHTML = "Home";
+            document.getElementById('navbar-home').style.color = '#CE9C29';
             break;
         case 'Learn more about me': //Esto hace que esta opcion tambien dentra al about
         case 'About':
             document.getElementById('div-about').style.display = "block";
             document.getElementById('section-name').innerHTML = "About Me";
+            document.getElementById('navbar-about').style.color = '#CE9C29';
             break;
         case 'See my Portfolio':
         case 'Portfolio':
             document.getElementById('div-portfolio').style.display = "block";
             document.getElementById('section-name').innerHTML = "My Portfolio";
+            document.getElementById('navbar-portfolio').style.color = '#CE9C29';
             break;
         case 'Blog':
             document.getElementById('div-blog').style.display = "block";
             document.getElementById('section-name').innerHTML = "My Blog";
+            document.getElementById('navbar-blog').style.color = '#CE9C29';
             break;
         case 'Contact':
             document.getElementById('section-name').innerHTML = "Contact Me";
+            document.getElementById('navbar-contact').style.color = '#CE9C29';
             document.getElementById('div-contact').style.display = "block";
             break;
         default:
             document.getElementById('section-name').innerHTML = "Home";
             document.getElementById('div-home').style.display = "flex";
+            document.getElementById('navbar-home').style.color = '#CE9C29';
             break;
     }
 }
